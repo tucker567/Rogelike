21064139616072c448077555ff8251311d83dc1d
tucker567 <tuckerstevens08@gmail.com>
2025-04-02 21:44:57 -0600

    1/2 of wish bone code

--- Assets/Chests and Items/Chest Button.cs
+++ Assets/Chests and Items/Chest Button.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System.Collections;
+using System.Linq;
 
 
 public class ChestInteraction : MonoBehaviour
--- Assets/Chests and Items/ItemsScripts/SpeedBoost.cs
+++ Assets/Chests and Items/ItemsScripts/SpeedBoost.cs
@@ -1,19 +1,13 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "SpeedBoostEffect", menuName = "Items/Effects/Speed Boost")]
-public class SpeedBoostEffect : ScriptableObject, ItemEffects
+[CreateAssetMenu(fileName = "MoreChestDrop", menuName = "Items/Effects/More Chest Drop")]
+public class MoreChestDrop : ScriptableObject, ItemEffects
 {
     public float maxBonus = 1.0f;       // +100% max bonus
     public float falloffRate = 0.5f;    // Higher = faster falloff
 
     public void ApplyEffect(GameObject player, int stackCount)
     {
-        if (stackCount <= 0) return;
 
-        var stats = PlayerStatsEffects.Instance;
-        float baseSpeed = stats.moveSpeed;
-
-        float bonusMultiplier = 1 + (maxBonus * (1 - Mathf.Exp(-falloffRate * stackCount)));
-        stats.finalMoveSpeed = baseSpeed * bonusMultiplier;
     }
 }
