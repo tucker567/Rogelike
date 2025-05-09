8683ec7b17d5f9f9c0cf66fc5bfd5ac31f9a9c3f
tucker567 <tuckerstevens08@gmail.com>
2025-05-09 11:18:33 -0600

    Quality of Life
    -  items rotate based off of left and right speed
    - removed unnecessary map setting from character select

--- Assets/Map generator/PerlinSettings.cs
+++ Assets/Map generator/PerlinSettings.cs
@@ -3,10 +3,6 @@ using UnityEngine;
 public class PerlinSettings : MonoBehaviour
 {
     public static PerlinSettings Instance { get; private set; }
-
-    public float magnitude = 10.0f;
-    public float frequency = 1.0f;
-    public float noiseThreshold = 0.5f;
     public int seed = -1;
 
     void Awake()
